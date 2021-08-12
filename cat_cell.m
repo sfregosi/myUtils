de1966b4b0fba90641527bfc5cc2fe2c22d98372
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-08-12 17:41:40 -0400

    Create cat_cell.m
    copied from Triton

---
+++ cat_cell.m
@@ -0,0 +1,9 @@
+function c1 = cat_cell(c1, c2)
+% concatenate two cell arrays cause APPARENTLY THIS ISN'T EASY IN MATLAB
+% originally found in Triton code. Extracted and added to myUtils 
+% S. Fregosi 2021 08 11
+
+for k = 1:size(c2, 2)
+    c1{end+1} = c2{k};
+end
+end
