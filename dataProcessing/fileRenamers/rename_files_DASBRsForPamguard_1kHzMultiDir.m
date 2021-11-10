c5c2827c3cc9d922cbb9c32e57ef60c43f706875
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-11-10 18:28:19 -0500

    rename dasbr data

---
+++ dataProcessing/fileRenamers/rename_files_DASBRsForPamguard_1kHzMultiDir.m
@@ -0,0 +1,22 @@
+%Convert file names for LTSA in Triton
+
+
+path_data = 'S:\MACS_2021_DASBR\decimated\1kHz\';
+folders = dir(path_data);
+for g = 3:length(folders)
+    
+    folderName = folders(g).name;
+    path_out = [path_data folderName '\'];
+    % mkdir(path_out);
+    
+    files = dir([path_data folderName '\*_1kHz.wav']);
+    for f = 1:length(files)
+        % old name
+        [pathname, filename, extension] = fileparts(files(f).name);
+        % new name
+        newName = ['1kHz_' filename(1:end-5)];
+        % rename
+        movefile([path_data folderName '\' filename extension], ...
+            [path_out newName extension]);
+    end
+end
---
+++ dataProcessing/fileRenamers/rename_files_DASBRsForPamguard_9_6kHzMultiDir.m
@@ -0,0 +1,22 @@
+%Convert file names for LTSA in Triton
+
+
+path_data = 'S:\MACS_2021_DASBR\decimated\9.6kHz\';
+folders = dir(path_data);
+for g = 3:length(folders)
+    
+    folderName = folders(g).name;
+    path_out = [path_data folderName '\'];
+    % mkdir(path_out);
+    
+    files = dir([path_data folderName '\*_9.6kHz.wav']);
+    for f = 1:length(files)
+        % old name
+        [pathname, filename, extension] = fileparts(files(f).name);
+        % new name
+        newName = ['9-6kHz_' filename(1:end-7)];
+        % rename
+        movefile([path_data folderName '\' filename extension], ...
+            [path_out newName extension]);
+    end
+end
