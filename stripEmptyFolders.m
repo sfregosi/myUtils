cec0fceae8df25cc9f8885a989192ab297c071d6
sfregosi <22456126+sfregosi@users.noreply.github.com>
2022-10-17 14:43:03 -0700

    added documentation and remove any non dirs

--- stripEmptyFolders.m
+++ stripEmptyFolders.m
@@ -1,15 +1,35 @@
-function [dirStructNew, folderNames] = stripEmptyFolders(dirStruct)
-
-% get the folder names for a directory with '.' and '..' removed
+function [folderStruct, folderNames] = stripEmptyFolders(dirStruct)
+% STRIPEMPTYFOLDERS     remove '.' and '..' folders from a 'dir' output
 %
-%
-% INPUT:    dirStruct       structure output typically from a dir() command
+%   Syntax:
+%       [FOLDERSTRUCT, FOLDERNAMES] = STRIPEMPTYFOLDERS(DIRSTRUCT)
 % 
-% OUTPUT:   dirStructNew    dirStruct without '.' and '..' rows
-%           folderNames     names of folders as cell array
+%   Description:
+%       get the folder names and a cleaned up folder structure object for a 
+%       directory call with 'dir', removing the empty holders '.' and '..'
+%
+%   Inputs:
+%       dirStruct       structure output typically from a dir() command
+% 
+%   Outputs:
+%       dirStructNew    dirStruct without '.' and '..' rows
+%       folderNames     names of folders as cell array
+%
+%   Examples:
+%
+%   See also
+%       dir, stripEmpties
+% 
+%	Authors:
+%		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%	Created with MATLAB ver.: 9.9.0.1524771 (R2020b) Update 2
+%
+%	FirstVersion: 	unknown
+%	Updated:        17 October 2022
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-folderNames = {dirStruct([dirStruct.isdir]).name}';
-dirStructNew = dirStruct(~ismember(folderNames, {'.','..'}));
-folderNames = folderNames(~ismember(folderNames, {'.','..'}));
+folderStruct = dirStruct([dirStruct.isdir],:);
+folderStruct = folderStruct(~ismember({folderStruct.name}, {'.','..'}));
+folderNames = {folderStruct(:).name}';
 
 end
