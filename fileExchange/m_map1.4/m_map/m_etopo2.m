dc0958864a3f1229e2af4346a03470c89966e36b
sfregosi <22456126+sfregosi@users.noreply.github.com>
2022-12-29 16:17:37 -0800

    mods to accept PATHNAME as varargin

--- fileExchange/m_map1.4/m_map/m_etopo2.m
+++ fileExchange/m_map1.4/m_map/m_etopo2.m
@@ -4,10 +4,10 @@ function [values,longs,lats]=m_etopo2(varargin)
 %        M_ETOPO2(OPTN) where OPTN is one of the following strings lets
 %        you draw a particular background:
 %        OPTN: 'contour' -  contour lines are drawn.
-%              'contourf' -  filled contours are drawn. 
+%              'contourf' -  filled contours are drawn.
 %                               LEVELS are the levels used, and ARGS
-%                               are optional patch arguments of line types, 
-%                               colors, etc. 
+%                               are optional patch arguments of line types,
+%                               colors, etc.
 %              'pcolor'    - pcolor call
 %              'image'     - displays pixellated image
 %              'shadedrelief' - shaded relief map.
@@ -17,7 +17,7 @@ function [values,longs,lats]=m_etopo2(varargin)
 %        contour/contourf call.
 %
 %        [ELEV,LONG,LAT]=M_ETOPO2([LONG_MIN LONG_MAX LAT_MIN LAT_MAX])
-%        extracts elevation data for the given lat/long limits (without 
+%        extracts elevation data for the given lat/long limits (without
 %        plotting).
 %
 %        Can be configured for etopo2v2 and etopo1, if you want to use
@@ -38,18 +38,25 @@ function [values,longs,lats]=m_etopo2(varargin)
 % 21/Mar/06 - modified for etopo2
 % 4/DEc/11 - isstr to ischar
 % May/28/2014 - modified for etopo2v2 (which has actualy ben around for years...)
-% May/29/2014 - modified for etopo1  
+% May/29/2014 - modified for etopo1
 % Mar/20/2019 - changed call sequence, other stuff for shaded relief.
+% Dec/29/2022 - allow to specify PATHNAME as varargin
 
 
 %%% This will have to be set by YOU the USER! ---------------------
 
 %PATHNAME='/ocean/rich/more/mmapbase/etopo2v2/';   % Be sure to end the path with a "/" or
-PATHNAME='C:\Users\Selene.Fregosi\Documents\GIS\etopo1\';   % Be sure to end the path with a "/" or
-                                                 % whatever your separator is.
+if ~any(strcmp(varargin, 'PATHNAME'))
+    PATHNAME='C:\Users\Selene.Fregosi\Documents\GIS\etopo1\';   % Be sure to end the path with a "/" or
+    % whatever your separator is.
+elseif any(strcmp(varargin, 'PATHNAME'))
+    pnIdx = find(strcmp(varargin, 'PATHNAME'));
+    PATHNAME = varargin{pnIdx+1};
+    varargin(pnIdx:pnIdx+1) = [];
+end
 %  Note - etopo2v2 now comes in 4 flavours - both grid-centered
 % and cell-centered, with both in either big-endian or little-endian
-% formats. Use the grid-centered version, 
+% formats. Use the grid-centered version,
 % and right now I have used the big-endian version.
 %
 
@@ -59,8 +66,7 @@ PATHNAME='C:\Users\Selene.Fregosi\Documents\GIS\etopo1\';   % Be sure to end the
 
 %efid=fopen([PATHNAME 'ETOPO2v2g_i2_MSB.bin'],'r','b'); % in big-endian format
 %efid=fopen([PATHNAME 'ETOPO2v2c_i2_MSB.bin'],'r','b'); % in big-endian format
-efid=fopen([PATHNAME 'etopo1_ice_g_i2.bin'],'r','l'); % apparently little-endian format
-
+efid=fopen(fullfile(PATHNAME, 'etopo1_ice_g_i2.bin'),'r','l'); % apparently little-endian format
 % Now, specify whether this file is grid or cell-referenced
 grid=1; % 1 for grid reference, 0 for cell - but I haven't gotten cell reference working yet
 
@@ -82,7 +88,7 @@ decmax=800;
 
 
 
-%%% Don't change anything below this... 
+%%% Don't change anything below this...
 
 ptsperdeg=60/resolution;
 nx=360*(ptsperdeg);  % Grid width
@@ -91,17 +97,17 @@ ny=180*(ptsperdeg);  % Grid width
 
 
 if efid==-1
- warning(['Cannot open ' PATHNAME 'etopo2 !! \n   Have you installed the Etopo2 database correctly?' ...
+    warning(['Cannot open ' PATHNAME 'etopo2 !! \n   Have you installed the Etopo2 database correctly?' ...
         '\n   This (optional) database must be installed separately - see the M_Map user''s guide for instructions' ...
-	    '\n   ----Using default elevation database instead']);
- if nargout==0
-   m_elev(varargin{:});
-  elseif nargout==2
-   [values,longs]=m_elev(varargin{:});
-  elseif nargout==3	
-   [values,longs,lats]=m_elev(varargin{:});
- end	
-  return;
+        '\n   ----Using default elevation database instead']);
+    if nargout==0
+        m_elev(varargin{:});
+    elseif nargout==2
+        [values,longs]=m_elev(varargin{:});
+    elseif nargout==3
+        [values,longs,lats]=m_elev(varargin{:});
+    end
+    return;
 end
 
 
@@ -109,199 +115,199 @@ global MAP_PROJECTION MAP_VAR_LIST
 
 % First case if we just want to extract dat, otherwise we are drawing.
 if nargin==1 && ~ischar(varargin{1}) && length(varargin{1})==4
-  draw_map=0;
+    draw_map=0;
 else
-  draw_map=1;
-  optn='contour';
-  levels=[-7000:1000:-1000 1000:1000:5000];
-  
-  if nargin>=1 && ischar(varargin{1})
-     optn=varargin{1};
-     varargin(1)=[];
-  end
+    draw_map=1;
+    optn='contour';
+    levels=[-7000:1000:-1000 1000:1000:5000];
+    
+    if nargin>=1 && ischar(varargin{1})
+        optn=varargin{1};
+        varargin(1)=[];
+    end
 end
 
 
 
 if draw_map==1 && isempty(MAP_PROJECTION)
-  disp('No Map Projection initialized - call M_PROJ first!');
-  return;
+    disp('No Map Projection initialized - call M_PROJ first!');
+    return;
 end
 
 % This finds the nearest cell boundaries
 
 if grid
-
-   if draw_map
-
-     blat=floor(MAP_VAR_LIST.lats(1)*ptsperdeg);
-     tlat=ceil(MAP_VAR_LIST.lats(2)*ptsperdeg);
-     llong=floor(MAP_VAR_LIST.longs(1)*ptsperdeg);
-     rlong=ceil(MAP_VAR_LIST.longs(2)*ptsperdeg);
-
-     lngdec=ceil((rlong-llong+1)/decmax);
-     latdec=ceil((tlat-blat+1)/decmax);
-
-   else
-
-     blat=floor(varargin{1}(3)*ptsperdeg);
-     tlat=ceil(varargin{1}(4)*ptsperdeg);
-     llong=floor(varargin{1}(1)*ptsperdeg);
-     rlong=ceil(varargin{1}(2)*ptsperdeg);
-     lngdec=1;
-     latdec=1;
-
-   end
-
- 
-   lgs=        [llong:rlong]/ptsperdeg;   
-   lts=fliplr( [blat:tlat]/ptsperdeg    );  
-
-   ptsperline=nx+1;
-   
+    
+    if draw_map
+        
+        blat=floor(MAP_VAR_LIST.lats(1)*ptsperdeg);
+        tlat=ceil(MAP_VAR_LIST.lats(2)*ptsperdeg);
+        llong=floor(MAP_VAR_LIST.longs(1)*ptsperdeg);
+        rlong=ceil(MAP_VAR_LIST.longs(2)*ptsperdeg);
+        
+        lngdec=ceil((rlong-llong+1)/decmax);
+        latdec=ceil((tlat-blat+1)/decmax);
+        
+    else
+        
+        blat=floor(varargin{1}(3)*ptsperdeg);
+        tlat=ceil(varargin{1}(4)*ptsperdeg);
+        llong=floor(varargin{1}(1)*ptsperdeg);
+        rlong=ceil(varargin{1}(2)*ptsperdeg);
+        lngdec=1;
+        latdec=1;
+        
+    end
+    
+    
+    lgs=        [llong:rlong]/ptsperdeg;
+    lts=fliplr( [blat:tlat]/ptsperdeg    );
+    
+    ptsperline=nx+1;
+    
 else   % Cell centering
-
-   if draw_map
-
-     blat=floor(MAP_VAR_LIST.lats(1)*ptsperdeg-1/2);
-     tlat=ceil(MAP_VAR_LIST.lats(2)*ptsperdeg-1/2);
-     llong=floor(MAP_VAR_LIST.longs(1)*ptsperdeg-1/2);
-     rlong=ceil(MAP_VAR_LIST.longs(2)*ptsperdeg-1/2);
-
-     lngdec=ceil((rlong-llong-1)/decmax);
-     latdec=ceil((tlat-blat-1)/decmax);
-
-   else
-
-     blat=floor(varargin{1}(3)*ptsperdeg-1/2);
-     tlat=ceil(varargin{1}(4)*ptsperdeg-1/2);
-     llong=floor(varargin{1}(1)*ptsperdeg-1/2);
-     rlong=ceil(varargin{1}(2)*ptsperdeg-1/2);
-     lngdec=1;
-     latdec=1;
-
-   end
-
-   % Cell centers are moved 1/60 = .5/30 awa from the cell boundaries
-   %
-   % (1,1) point is at -180W, 90N.
-
-   lgs=([llong:rlong]+1/2)/ptsperdeg;  % move right
-   lts=fliplr( ([blat:tlat]-1/2)/ptsperdeg );  % move down
-
-   ptsperline=nx;
-end   
+    
+    if draw_map
+        
+        blat=floor(MAP_VAR_LIST.lats(1)*ptsperdeg-1/2);
+        tlat=ceil(MAP_VAR_LIST.lats(2)*ptsperdeg-1/2);
+        llong=floor(MAP_VAR_LIST.longs(1)*ptsperdeg-1/2);
+        rlong=ceil(MAP_VAR_LIST.longs(2)*ptsperdeg-1/2);
+        
+        lngdec=ceil((rlong-llong-1)/decmax);
+        latdec=ceil((tlat-blat-1)/decmax);
+        
+    else
+        
+        blat=floor(varargin{1}(3)*ptsperdeg-1/2);
+        tlat=ceil(varargin{1}(4)*ptsperdeg-1/2);
+        llong=floor(varargin{1}(1)*ptsperdeg-1/2);
+        rlong=ceil(varargin{1}(2)*ptsperdeg-1/2);
+        lngdec=1;
+        latdec=1;
+        
+    end
+    
+    % Cell centers are moved 1/60 = .5/30 awa from the cell boundaries
+    %
+    % (1,1) point is at -180W, 90N.
+    
+    lgs=([llong:rlong]+1/2)/ptsperdeg;  % move right
+    lts=fliplr( ([blat:tlat]-1/2)/ptsperdeg );  % move down
+    
+    ptsperline=nx;
+end
 
 % Extract topographhy for the desired lat/long limits
 
 eaxes=[llong+nx/2 rlong+nx/2 ny/2-blat ny/2-tlat];  % indexes of edges (start with 0)
 
-% Get it inside, or just off the right edge if edge-crossing   
+% Get it inside, or just off the right edge if edge-crossing
 if eaxes(2)>nx,  eaxes([1 2])=eaxes([1 2])-nx; end
 if eaxes(1)<0,   eaxes([1 2])=eaxes([1 2])+nx; end
 
 
 
 if (eaxes(2)>nx  )   % Read it in in 2 pieces!
-
-
-  nlat=round((eaxes(3)-eaxes(4)))+1;
-  nlgr=round( eaxes(2)-nx )+1;
-  nlgl=round(nx-eaxes(1)); %%%nlng-nlgr
-  nlng=nlgr+nlgl;
-
-  values=zeros(nlat,nlng);
-  for ii=[1:nlat]
-   fseek(efid,( (ii-1+eaxes(4))*ptsperline )*2,'bof');
-   values(ii,nlng+[-nlgr:-1]+1)=fread(efid,[1 nlgr],'int16');
-   fseek(efid,( (ii-1+eaxes(4))*ptsperline+eaxes(1) )*2,'bof');
-   values(ii,1:nlgl)=fread(efid,[1 nlgl],'int16');
-  end
-
+    
+    
+    nlat=round((eaxes(3)-eaxes(4)))+1;
+    nlgr=round( eaxes(2)-nx )+1;
+    nlgl=round(nx-eaxes(1)); %%%nlng-nlgr
+    nlng=nlgr+nlgl;
+    
+    values=zeros(nlat,nlng);
+    for ii=[1:nlat]
+        fseek(efid,( (ii-1+eaxes(4))*ptsperline )*2,'bof');
+        values(ii,nlng+[-nlgr:-1]+1)=fread(efid,[1 nlgr],'int16');
+        fseek(efid,( (ii-1+eaxes(4))*ptsperline+eaxes(1) )*2,'bof');
+        values(ii,1:nlgl)=fread(efid,[1 nlgl],'int16');
+    end
+    
 else  % Read it in one piece
-
-  nlat=round((eaxes(3)-eaxes(4)))+1;
-  nlng=round((eaxes(2)-eaxes(1)))+1;
-  values=zeros(nlat,nlng);
-  for ii=[1:nlat]
-   fseek(efid,( (ii-1 +eaxes(4))*ptsperline +eaxes(1) )*2,'bof');
-   values(ii,:)=fread(efid,[1 nlng],'int16');
-  end
-
+    
+    nlat=round((eaxes(3)-eaxes(4)))+1;
+    nlng=round((eaxes(2)-eaxes(1)))+1;
+    values=zeros(nlat,nlng);
+    for ii=[1:nlat]
+        fseek(efid,( (ii-1 +eaxes(4))*ptsperline +eaxes(1) )*2,'bof');
+        values(ii,:)=fread(efid,[1 nlng],'int16');
+    end
+    
 end
-fclose(efid); 
- 
+fclose(efid);
+
 
 if draw_map
-
-  % Set current projection to geographic
-  Currentmap=m_coord('set');
-  m_coord('geographic');
-  
-  % If decimating doesn't get the end point on longitude, then manually 
-  % add it.
-  if rem((length(lgs)-1)/lngdec,1)==0
-      topo=values(1:latdec:end,1:lngdec:end);
-      lts=lts(1:latdec:end);
-      lgs=lgs(1:lngdec:end);
-  else
-      topo=values(1:latdec:end,[1:lngdec:end end]);
-      lts=lts(1:latdec:end);
-      lgs=lgs([1:lngdec:end end]);
-  end
-  
-  if all(levels<0)
-   topo=-topo;
-   levels=-levels;
-  end
-  
-  hold on;
-  switch optn
-   case 'contour'
-       if ~isempty(varargin) && ~ischar(varargin{1})
-           levels=varargin{1};
-           varargin(1)=[];
-       end
-      [values,longs]=m_contour(lgs,lts,topo,levels);
-      if ~isempty(varargin)
-          for l=1:length(longs) 
-              set(longs(l),varargin{:}); 
-          end 
-      end
-   case 'contourf'
-       if ~isempty(varargin) && ~ischar(varargin{1})
-           levels=varargin{1};
-           varargin(1)=[];
-       end
-      [values,longs]=m_contourf(lgs,lts,topo,levels);
-      if ~isempty(varargin)
-          for l=1:length(longs) 
-              set(longs(l),varargin{:}); 
-          end 
-      end
-   case 'pcolor'
-      [longs]=m_pcolor(lgs,lts,topo,varargin{:});
-   case 'image'
-      [longs]=m_image(lgs,lts,topo,varargin{:});
-   case 'shadedrelief'
-       [Im,X,Y]=m_image(lgs,lts,topo);
-       [longs]=m_shadedrelief(X,Y,Im,'coords','map','gradient',3,varargin{:});
-   otherwise
-      error(['m_elev:Unrecognized option: ' optn]);       
-  end  
-
-  set(longs,'tag','m_elev');  
-  
-  % Reset map coords
- m_coord(Currentmap.name);
-
+    
+    % Set current projection to geographic
+    Currentmap=m_coord('set');
+    m_coord('geographic');
+    
+    % If decimating doesn't get the end point on longitude, then manually
+    % add it.
+    if rem((length(lgs)-1)/lngdec,1)==0
+        topo=values(1:latdec:end,1:lngdec:end);
+        lts=lts(1:latdec:end);
+        lgs=lgs(1:lngdec:end);
+    else
+        topo=values(1:latdec:end,[1:lngdec:end end]);
+        lts=lts(1:latdec:end);
+        lgs=lgs([1:lngdec:end end]);
+    end
+    
+    if all(levels<0)
+        topo=-topo;
+        levels=-levels;
+    end
+    
+    hold on;
+    switch optn
+        case 'contour'
+            if ~isempty(varargin) && ~ischar(varargin{1})
+                levels=varargin{1};
+                varargin(1)=[];
+            end
+            [values,longs]=m_contour(lgs,lts,topo,levels);
+            if ~isempty(varargin)
+                for l=1:length(longs)
+                    set(longs(l),varargin{:});
+                end
+            end
+        case 'contourf'
+            if ~isempty(varargin) && ~ischar(varargin{1})
+                levels=varargin{1};
+                varargin(1)=[];
+            end
+            [values,longs]=m_contourf(lgs,lts,topo,levels);
+            if ~isempty(varargin)
+                for l=1:length(longs)
+                    set(longs(l),varargin{:});
+                end
+            end
+        case 'pcolor'
+            [longs]=m_pcolor(lgs,lts,topo,varargin{:});
+        case 'image'
+            [longs]=m_image(lgs,lts,topo,varargin{:});
+        case 'shadedrelief'
+            [Im,X,Y]=m_image(lgs,lts,topo);
+            [longs]=m_shadedrelief(X,Y,Im,'coords','map','gradient',3,varargin{:});
+        otherwise
+            error(['m_elev:Unrecognized option: ' optn]);
+    end
+    
+    set(longs,'tag','m_elev');
+    
+    % Reset map coords
+    m_coord(Currentmap.name);
+    
 else
-
-  [longs,lats]=meshgrid(lgs,lts);
-
+    
+    [longs,lats]=meshgrid(lgs,lts);
+    
 end
 
 
 if nargout==0
- clear values longs lats
+    clear values longs lats
 end
