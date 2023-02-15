27b8a7d1335b1489d75b4e2f077dfdd6173a868e
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-02-15 08:56:15 -0800

    added better documentation

--- stripEmpties.m
+++ stripEmpties.m
@@ -1,12 +1,32 @@
 function [dirStructNew, names] = stripEmpties(dirStruct)
-
-% get the folder/file names for a directory with '.' and '..' removed
+% STRIPEMPTIES     remove '.' and '..' files from a 'dir' output
 %
-%
-% INPUT:    dirStruct       structure output typically from a dir() command
+%   Syntax:
+%       [DIRSTRUCTNEW, NAMES] = STRIPEMPTIES(DIRSTRUCT)
 % 
-% OUTPUT:   dirStructNew    dirStruct without '.' and '..' rows
-%           names           names of folders/files as cell array
+%   Description:
+%       get the file names and a cleaned up structure object for a 
+%       directory call with 'dir', removing any empty files '.' and '..'
+%
+%   Inputs:
+%       dirStruct       structure output typically from a dir() command
+% 
+%   Outputs:
+%       dirStructNew    dirStruct without '.' and '..' rows
+%       names           names of folders/files as cell array
+%
+%   Examples:
+%
+%   See also
+%       dir, stripEmptyFolders
+% 
+%	Authors:
+%		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%	Created with MATLAB ver.: 9.9.0.1524771 (R2020b) Update 2
+%
+%	FirstVersion: 	unknown
+%	Updated:        13 February 2023
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 names = {dirStruct.name}';
 dirStructNew = dirStruct(~ismember(names, {'.','..'}));
