3093e4b1a5c7f763494019a9f922aae713c0c948
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-07-31 17:47:13 -0700

    Create stitchWavs.m
    moving over from tagless-acoustics utils

---
+++ stitchWavs.m
@@ -0,0 +1,46 @@
+function [sig, fs] = stitchWavs(wavFiles)
+% STITCHWAVS	Combine multiple wav files into a single variable
+%
+%   Syntax:
+%       [SIG, FS] = STITCHWAVS(WAVFILES)
+%
+%   Description:
+%       Read in multiple wav files sequentially and combine them into a
+%       single 'sig' variable for plotting, etc. 
+% 
+%   Inputs:
+%       wavFiles   [STRUCT] output of a dir command containing fields
+%       'folder' and 'name'
+%
+%	Outputs:
+%       sig  [matrix] N-by-1 matrix of signal values
+%       fs   [integer] sampling rate as read by audioinfo
+%
+%   Examples:
+%       wavFiles = dir('sounds\*.wav');
+%       [sig, fs] = stitchWavs(wavFiles(1:5));
+%
+%   See also DIR AUDIOREAD AUDIOINFO
+%
+%   Authors:
+%       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%
+%   FirstVersion:   31 July 2023
+%   Updated:
+%
+%   Created with MATLAB ver.: 9.10.0.1739362 (R2021a) Update 5
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+sig = [];
+if ~isempty(wavFiles)
+    for f = 1:length(wavFiles)
+        try
+            [y, fs] = audioread(fullfile(wavFiles(f).folder, wavFiles(f).name));
+            sig = [sig; y];
+        catch
+            fprintf(1, '%s is corrupt\n', wavFiles(f).name);
+        end
+    end
+
+end
