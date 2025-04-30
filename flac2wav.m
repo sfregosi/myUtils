86cbcb07ed252c397a90a2ac615659b5e32bdbd6
selenefregosi-noaa <selene.fregosi@noaa.gov>
2025-04-30 12:08:59 -0700

    updated help docs, added arg checks

--- flac2wav.m
+++ flac2wav.m
@@ -1,63 +1,94 @@
-function output = flac2wav(flacDir, inDir, outDir)
-% FLAC2WAV	One-line description here, please
+function flac2wav(path_flac, inDir, outDir)
+% FLAC2WAV	Wrapper to convert FLAC to WAV via command line/flac.exe
 %
 %   Syntax:
-%       OUTPUT = FLAC2WAV(FLACDIR, INDIR, OUTDIR)
+%       FLAC2WAV(PATH_FLAC, INDIR, OUTDIR)
 %
 %   Description:
-%       Detailed description here, please
+%       Wrapper function to convert a single directory of FLAC files to WAV
+%       format using the command line to call flac.exe. flac.exe can be
+%       downloaded from: https://xiph.org/flac/download.html
+%
 %   Inputs:
-%       flacDir   describe, please
-%       inDir   describe, please
-%       outDir   describe, please
+%       path_flac   [string] fullfile path to flac.exe but should not
+%                   include the '.exe.', e.g.,
+%                   'C:\flac-1.5.0-win\Win64\flac'
+%       inDir       [string] path to folder containing FLAC files to be
+%                   converted
+%       outDir      [string] path to output folder to store newly created
+%                   WAV files. *THIS PATH MUST ENDED IN A FINAL SLASH!!* A
+%                   check will ensure that it does.
 %
 %	Outputs:
-%       output  describe, please
+%       None. Creates files in outDir
 %
 %   Examples:
+%       wav2flac('C:\user.name\programs\flac-1.5.0-win\Win64\flac', ...
+%                'F:\flacFiles', 'F:\wavFiles\');
 %
-%   See also
+%   See also WAV2FLAC
 %
 %   Authors:
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
-%   Updated:   2025 April 22
+%   Updated:   2025 April 30
 %
 %   Created with MATLAB ver.: 24.2.0.2740171 (R2024b) Update 1
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ADD IN ARGUMENT CHECKS
+% argument checks
 
-% set input folder containing flac files
-inDir = 'F:\CalCurCEAS2024_glider_data\flac_tests\test_flacs';
-cd(inDir)
-% set output directory to put new files - VERY IMPORTANT TO HAVE FINAL SLASH \
-outDir = 'F:\CalCurCEAS2024_glider_data\flac_tests\output_wavs\';
+% path to flac.exe (without the .exe)
+% e.g., path_flac = 'C:\Users\selene.fregosi\programs\flac-1.5.0-win\Win64\flac';
+if ~exist([path_flac, '.exe'], 'file')
+    [file, location] = uigetfile('*.exe', 'Select flac.exe', path_flac);
+    if ~strcmp(file, 'flac.exe')
+        error('flac.exe not properly selected. Exiting.')
+    end
+    path_flac = fullfile(location, 'flac');
+end
 
-% set location of FLAC program
-fullPathFlac = 'C:\Users\selene.fregosi\programs\flac-1.5.0-win\Win64\flac';
+% check input directory
+% e.g., inDir = 'F:\CalCurCEAS2024_glider_data_flac';
+if ~isfolder(inDir)
+    inDir = uigetdir('C:\', 'Select folder containing FLAC files');
+end
+    fprintf(1, 'inDir: %s\n', inDir);
 
-% process
+% check output directory
+% VERY IMPORTANT TO HAVE FINAL SLASH \
+% outDir = 'F:\CalCurCEAS2024_glider_data\wav\';
+if ~isfolder(outDir)
+    outDir = uigetdir(inDir, 'Select folder to write WAV files to');
+end
+% make sure it has an ending slash
+if ~endsWith(outDir, filesep)
+    outDir = [outDir, filesep];
+end
+fprintf(1, 'outDir: %s\n', outDir);
 
+
+% process the files
 tic
 
-% build out command line string
-encodeStr = '%s --decode --keep-foreign-metadata-if-present --output-prefix=%s %s';
+% % make output directory if it doesn't exist - catch above replaces this
+% if ~isfolder(outDir)
+%     mkdir(outDir)
+% end
 
-% make output directory if it doesn't exist
-if ~isfolder(outDir)
-    mkdir(outDir)
-end
+% build out command line string
+decodeStr = '%s --decode --keep-foreign-metadata-if-present --output-prefix=%s %s';
 
 % loop through all files and convert
 fileList = dir(fullfile(inDir, '*.flac'));
 if isempty(fileList)
-    return
+    error('No FLAC files found in %s', inDir);
 end
+
 cd(inDir)
 nFiles = length(fileList);
 for iFile = 1:length(fileList)
-    myCMD = sprintf(encodeStr, fullPathFlac, outDir, fileList(iFile).name);
+    myCMD = sprintf(decodeStr, path_flac, outDir, fileList(iFile).name);
     [status, cmdout] = system(char(myCMD));
     fprintf('Done with file %0.0f of %0.0f - %s\n', ...
         iFile, nFiles, fileList(iFile).name)
--- wav2flac.m
+++ wav2flac.m
@@ -1,63 +1,94 @@
-function output = wav2flac(flacDir, inDir, outDir)
-% WAV2FLAC	One-line description here, please
+function wav2flac(path_flac, inDir, outDir)
+% WAV2FLAC	Wrapper to convert WAV to FLAC via command line/flac.exe
 %
 %   Syntax:
-%       OUTPUT = WAV2FLAC(FLACDIR, INDIR, OUTDIR)
+%       WAV2FLAC(PATH_FLAC, INDIR, OUTDIR)
 %
 %   Description:
-%       Detailed description here, please
+%       Wrapper function to convert a single directory of WAV files to FLAC
+%       format using the command line to call flac.exe. flac.exe can be
+%       downloaded from: https://xiph.org/flac/download.html
+%
 %   Inputs:
-%       flacDir   describe, please
-%       inDir   describe, please
-%       outDir   describe, please
+%       path_flac   [string] fullfile path to flac.exe but should not
+%                   include the '.exe.', e.g.,
+%                   'C:\flac-1.5.0-win\Win64\flac'
+%       inDir       [string] path to folder containing WAV files to be
+%                   converted
+%       outDir      [string] path to output folder to store newly created
+%                   FLAC files. *THIS PATH MUST ENDED IN A FINAL SLASH!!* A
+%                   check will ensure that it does.
 %
 %	Outputs:
-%       output  describe, please
+%       None. Creates files in outDir
 %
 %   Examples:
+%       wav2flac('C:\user.name\programs\flac-1.5.0-win\Win64\flac', ...
+%                'F:\wavFiles', 'F:\flacFiles\');
 %
-%   See also
+%   See also FLAC2WAV
 %
 %   Authors:
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
-%   Updated:   2025 April 22
+%   Updated:   2025 April 30
 %
 %   Created with MATLAB ver.: 24.2.0.2740171 (R2024b) Update 1
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% ADD IN ARGUMENT CHECKS
+% argument checks
 
-% set input folder containing wav files
-inDir = 'F:\CalCurCEAS2024_glider_data\flac_tests\test_wavs';
-cd(inDir)
-% set output directory to put new files - VERY IMPORTANT TO HAVE FINAL SLASH \
-outDir = 'F:\CalCurCEAS2024_glider_data\flac_tests\output_flacs\';
+% path to flac.exe (without the .exe)
+% e.g., path_flac = 'C:\Users\selene.fregosi\programs\flac-1.5.0-win\Win64\flac';
+if ~exist([path_flac, '.exe'], 'file')
+    [file, location] = uigetfile('*.exe', 'Select flac.exe', path_flac);
+    if ~strcmp(file, 'flac.exe')
+        error('flac.exe not properly selected. Exiting.')
+    end
+    path_flac = fullfile(location, 'flac');
+end
 
-% set location of FLAC program
-fullPathFlac = 'C:\Users\selene.fregosi\programs\flac-1.5.0-win\Win64\flac';
+% check input directory
+% e.g., inDir = 'F:\CalCurCEAS2024_glider_data';
+if ~isfolder(inDir)
+    inDir = uigetdir('C:\', 'Select folder containing WAV files');
+end
+    fprintf(1, 'inDir: %s\n', inDir);
 
-% process
+% check output directory
+% VERY IMPORTANT TO HAVE FINAL SLASH \
+% outDir = 'F:\CalCurCEAS2024_glider_data\flac\';
+if ~isfolder(outDir)
+    outDir = uigetdir(inDir, 'Select folder to save created FLAC files to');
+end
+% make sure it has an ending slash
+if ~endsWith(outDir, filesep)
+    outDir = [outDir, filesep];
+end
+fprintf(1, 'outDir: %s\n', outDir);
 
+
+% process the files
 tic
+
+% % make output directory if it doesn't exist - catch above replaces this
+% if ~isfolder(outDir)
+%     mkdir(outDir)
+% end
 
 % build out command line string
 encodeStr = '%s --keep-foreign-metadata-if-present --output-prefix=%s %s';
 
-% make output directory if it doesn't exist
-if ~isfolder(outDir)
-    mkdir(outDir)
-end
-
 % loop through all files and convert
 fileList = dir(fullfile(inDir, '*.wav'));
 if isempty(fileList)
-    return
+    error('No WAV files found in %s', inDir);
 end
+
 cd(inDir)
 nFiles = length(fileList);
 for iFile = 1:length(fileList)
-    myCMD = sprintf(encodeStr, fullPathFlac, outDir, fileList(iFile).name);
+    myCMD = sprintf(encodeStr, path_flac, outDir, fileList(iFile).name);
     [status, cmdout] = system(char(myCMD));
     fprintf('Done with file %0.0f of %0.0f - %s\n', ...
         iFile, nFiles, fileList(iFile).name)
